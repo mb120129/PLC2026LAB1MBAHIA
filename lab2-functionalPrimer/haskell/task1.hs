993f46f2212a6fd565abfc7d7bef4f1f64c850d5
mb120129 <msbahia11@outlook.com>
2026-01-27 20:44:04 +0000

    solved error on task1

--- lab2-functionalPrimer/haskell/task1.hs
+++ lab2-functionalPrimer/haskell/task1.hs
@@ -4,8 +4,10 @@ main =
     do
     print (myFunc1 5)
     --print (MyFunc2 10)
+    print (func2 10)
     --print (3rdFunc)
+    print (func3rd)
 
 myFunc1 x = x*10
---MyFunc2 x = x*2
---3rdFunc = "Hello, this is 3rd Func"
+func2 x = x*2
+func3rd = "Hello, this is 3rd Func"
