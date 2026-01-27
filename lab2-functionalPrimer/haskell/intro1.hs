49176ebab3ef5ae3b9f650016197c43d3b40cc94
mb120129 <msbahia11@outlook.com>
2026-01-27 19:36:05 +0000

    changing values

--- lab2-functionalPrimer/haskell/intro1.hs
+++ lab2-functionalPrimer/haskell/intro1.hs
@@ -7,8 +7,8 @@ r2 = b^2 + 1/b where b = 2
 -- define a function:
 diff a b = abs (a - b)
 
-r3 = diff (diff a a) a  where a = 1
-r4 = diff (diff a b) b  where a = 1; b = 1
+r3 = diff (diff a a) a  where a = 2
+r4 = diff (diff a b) b  where a = 1; b = 2
 
 -- lists by enumeration
 r5 = [1,3,4,1]
