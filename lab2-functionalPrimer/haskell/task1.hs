0de3b6fe530e190ccecb49ad84da78688de2865c
mb120129 <msbahia11@outlook.com>
2026-01-27 20:45:09 +0000

    tested further on task1

--- lab2-functionalPrimer/haskell/task1.hs
+++ lab2-functionalPrimer/haskell/task1.hs
@@ -2,12 +2,12 @@ module Main where
 
 main =
     do
-    print (myFunc1 5)
+    print (_myFunc1 5)
     --print (MyFunc2 10)
-    print (func2 10)
+    print (_func2 10)
     --print (3rdFunc)
-    print (func3rd)
+    print (_Func3rd)
 
-myFunc1 x = x*10
-func2 x = x*2
-func3rd = "Hello, this is 3rd Func"
+_myFunc1 x = x*10
+_func2 x = x*2
+_Func3rd = "Hello, this is 3rd Func"
