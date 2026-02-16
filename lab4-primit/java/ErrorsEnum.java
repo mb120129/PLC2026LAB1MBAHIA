64b059c3730af5b2949768dfcee9edac2dd76c07
mb120129 <msbahia11@outlook.com>
2026-02-16 13:38:12 +0000

    working throough task 1b of lab 4

--- lab4-primit/java/ErrorsEnum.java
+++ lab4-primit/java/ErrorsEnum.java
@@ -54,16 +54,36 @@ public class ErrorsEnum
         return result;
     }
 
+    private static Error result2Error(Result r) {
+        Error error = null;
+
+        switch(r) {
+            case A_BIT_DIFFERENT:
+                error = Error.FP_ROUNDING;
+                break;
+            case INFINITY:
+                error = Error.FP_OVERFLOW;
+                break;
+            case ZERO:
+                error = Error.FP_UNDERFLOW;
+                break;
+            case VERY_DIFFERENT:
+                error = Error.INT_OVERFLOW;
+                break;
+        }
+        return error;
+    }
+
     public static void main(String[] args)
     {
-        System.out.print("Known errors = ");
-        for (Error e : EnumSet.allOf(Error.class)) 
+        System.out.print("Known results = ");
+        for (Result r : EnumSet.allOf(Result.class)) 
         {
             System.out.print(e + " ");
         }
         System.out.println();
         
-        Error e = getEnumElement("error", Error.class);
-        System.out.println(e + " results in: " + error2Result(e));
+        Result r = getEnumElement("result", Result.class);
+        System.out.println(e + " results from: " + result2Error(r));
     }
 }
